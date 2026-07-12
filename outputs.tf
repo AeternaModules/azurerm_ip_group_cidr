8259ed01b32ee99f0942251d3c4237e492ace351
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:02:37 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "ip_group_cidrs_id" {
+  description = "Map of id values across all ip_group_cidrs, keyed the same as var.ip_group_cidrs"
+  value       = { for k, v in azurerm_ip_group_cidr.ip_group_cidrs : k => v.id }
+}
 output "ip_group_cidrs_cidr" {
   description = "Map of cidr values across all ip_group_cidrs, keyed the same as var.ip_group_cidrs"
   value       = { for k, v in azurerm_ip_group_cidr.ip_group_cidrs : k => v.cidr }
